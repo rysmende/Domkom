b41153874a48039ddee2d5a7ff84c7e516b729c8
rysmende <44228198+rysmende@users.noreply.github.com>
2020-02-12 19:57:45 +0600

    Added more views

---
+++ Domkom/NewsCell.swift
@@ -0,0 +1,30 @@
+//
+//  NewsCell.swift
+//  Domkom
+//
+//  Created by Nurzhan Ababakirov on 2/12/20.
+//  Copyright © 2020 Nurzhan Ababakirov. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var numOfComs: UILabel!
+    @IBOutlet weak var NewsDate: UILabel!
+    @IBOutlet weak var newsTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Domkom/RequestCell.swift
@@ -0,0 +1,24 @@
+//
+//  RequestCell.swift
+//  Domkom
+//
+//  Created by Nurzhan Ababakirov on 2/12/20.
+//  Copyright © 2020 Nurzhan Ababakirov. All rights reserved.
+//
+
+import UIKit
+
+class RequestCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
